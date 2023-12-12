44e64e42d424922e7e86db55cffb121321abe537
Ebrahim P. Leite <brahim.sk9@gmail.com>
2023-12-11 22:48:31 -0300

    fixed mobile and another files

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let time = 2500,
-  currentImageIndex = 0,
-  images = document.querySelectorAll(".fotos img");
+    currentImageIndex = 0,
+    images = document.querySelectorAll(".fotos img");
 max = images.length;
 
 function nextImage() {
-  images[currentImageIndex].classList.remove("foto1");
-  currentImageIndex++;
-  if (currentImageIndex >= max) currentImageIndex = 0;
-  images[currentImageIndex].classList.add("foto1");
+    images[currentImageIndex].classList.remove("foto1");
+    currentImageIndex++;
+    if (currentImageIndex >= max) currentImageIndex = 0;
+    images[currentImageIndex].classList.add("foto1");
 }
 
-function start() {
-  setInterval(() => {
+setInterval(() => {
     nextImage();
-  }, time);
-}
-window.addEventListener("load", start);
+}, time);
